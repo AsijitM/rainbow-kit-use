73d3bcbe9d78508aec50c8b9f4fe04725f490844
AsijitM <asijit00@gmail.com>
2023-03-21 22:26:46 +0530

    first commit

---
+++ src/pages/_app.js
@@ -0,0 +1,34 @@
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
+import '../styles/globals.css';
+import '@rainbow-me/rainbowkit/styles.css';
+
+const { chains, provider } = configureChains(
+  [mainnet, polygon, optimism, arbitrum],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'Test web3 app',
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  provider,
+  connectors,
+});
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
+
+export default MyApp;
